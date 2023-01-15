9b9f7577c9ce2764439b0bbd24b7b802ed16129b
rtznprmpftl <kontakt@nicolas-berens.de>
2023-01-15 23:55:22 +0100

    initial commit of the monitor role

---
+++ roles/ff_monitor/files/create_node_list.py
@@ -0,0 +1,19 @@
+#!/bin/env python3
+import urllib.request,json
+
+with urllib.request.urlopen("https://hopglass.berlin.freifunk.net/nodes.json") as url:
+    data = json.loads(url.read().decode())
+
+    simplenodelist = list()
+
+    nodes = data.get('nodes')
+    for node in nodes:
+        simplenode = {
+            "latitude": node['nodeinfo']['location']['latitude'],
+            "longitude": node['nodeinfo']['location']['longitude'],
+            "name": node['nodeinfo']['hostname'],
+            "key": node['nodeinfo']['hostname']
+        }
+        simplenodelist.append(simplenode.copy())
+
+    print (json.dumps(simplenodelist, separators=(',', ':')))
