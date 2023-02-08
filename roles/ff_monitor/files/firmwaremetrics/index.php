197e33b01b2cf26069d1030d481a6b16576de10d
Nicolas Berens <kontakt@nicolas-berens.de>
2023-02-08 01:17:54 +0100

    add cgp, custom metrics and helper scripts

---
+++ roles/ff_monitor/files/firmwaremetrics/index.php
@@ -0,0 +1,46 @@
+<?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL ^ E_NOTICE);
+
+require_once('classify-firmware.php');
+header("Content-Type: text/plain");
+
+$url="https://hopglass.berlin.freifunk.net/nodes.json";
+//  Initiate curl
+$ch = curl_init();
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+
+// JSON as an object
+$json = json_decode($result);
+
+// firmwares
+$firmwares = array ('Other' => array(), 'OpenWrt' => array(), 'Freifunk Berlin' => array(), 'Kathleen' => array(),'Hedy' => array(), 'Falter' => array());
+
+foreach($json->nodes as $node) {
+    // Only use online nodes
+    if($node->flags->online === true) {
+        $firmware = classify_firmware($node->nodeinfo->software->firmware->release);
+        $firmwares[$firmware['name']][$firmware['version']]++; 
+    }
+    // echo $node->nodeinfo->software->firmware->release.': '.$firmware['name']." - " .$firmware['version']."\n";
+}
+
+// Output Exporter
+echo '# HELP freifunk_firmware_version_counter nodes with specific version'."\n";
+echo '# TYPE freifunk_firmware_version_counter gauge'."\n";
+foreach ($firmwares as $key => $val) {
+    ksort($firmwares[$key]);
+    foreach ($firmwares[$key] as $version => $count) {
+        echo 'freifunk_firmware_version_counter{name="'.$key.'",version="'.$version.'"} '.$count."\n";
+    }
+}
+
+// var_dump ($firmwares);
