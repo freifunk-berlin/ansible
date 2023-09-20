20033f03ad67d60a6a7f932c108f6e8ca0a94d20
Packet Please <pktpls@systemli.org>
2023-09-20 02:51:29 +0200

    fmt: run black and isort

--- roles/buildbot/files/notify.py
+++ roles/buildbot/files/notify.py
@@ -1,37 +1,38 @@
 # -*- python -*-
 # ex: set filetype=python:
 
-from buildbot.plugins import util, steps
+import pprint
+
+from buildbot.plugins import steps, util
+from buildbot.process.results import statusToString
 from buildbot.reporters.base import ReporterBase
 from buildbot.reporters.generators.build import BuildStartEndStatusGenerator
 from buildbot.reporters.generators.buildrequest import BuildRequestGenerator
 from buildbot.reporters.message import MessageFormatterRenderable
 from buildbot.util import httpclientservice
-from buildbot.process.results import statusToString
-
 from twisted.internet import defer
 from twisted.python import log
 
-import pprint
 
 class MatrixNotifier(ReporterBase):
-
     def __init__(self, homeserver=None, accessToken=None, room=None, **kwargs):
         self.homeserver = homeserver
         self.accessToken = accessToken
         self.room = room
         super().__init__(**kwargs)
 
-    def checkConfig(self, context=None, debug=None, verify=None, generators=None,
-                    **kwargs):
+    def checkConfig(
+        self, context=None, debug=None, verify=None, generators=None, **kwargs
+    ):
         if generators is None:
             generators = self._create_default_generators()
 
         super().checkConfig(generators=generators, **kwargs)
 
     @defer.inlineCallbacks
-    def reconfigService(self, context=None, debug=None,
-                        verify=None, generators=None, **kwargs):
+    def reconfigService(
+        self, context=None, debug=None, verify=None, generators=None, **kwargs
+    ):
         self.debug = debug
         self.verify = verify
         self.context = self.setup_context(context)
@@ -41,50 +42,62 @@ class MatrixNotifier(ReporterBase):
         yield super().reconfigService(generators=generators, **kwargs)
 
     def setup_context(self, context):
-        return context or util.Interpolate('buildbot/%(prop:buildername)s')
+        return context or util.Interpolate("buildbot/%(prop:buildername)s")
 
     # This is needed but I have no clue what it does.
     def _create_default_generators(self):
-        start_formatter = MessageFormatterRenderable('Build started.')
-        end_formatter = MessageFormatterRenderable('Build done.')
-        pending_formatter = MessageFormatterRenderable('Build pending.')
+        start_formatter = MessageFormatterRenderable("Build started.")
+        end_formatter = MessageFormatterRenderable("Build done.")
+        pending_formatter = MessageFormatterRenderable("Build pending.")
 
         return [
             BuildRequestGenerator(formatter=pending_formatter),
-            BuildStartEndStatusGenerator(start_formatter=start_formatter,
-                                         end_formatter=end_formatter)
+            BuildStartEndStatusGenerator(
+                start_formatter=start_formatter, end_formatter=end_formatter
+            ),
         ]
 
     @defer.inlineCallbacks
     def sendMessage(self, reports):
-        b = reports[0]['builds'][0]
-        builder = b['builder']['name']
+        b = reports[0]["builds"][0]
+        builder = b["builder"]["name"]
 
-        if 'number' not in b or b['complete'] != True or (builder != 'builds/targets' and builder != 'builds/packages'):
+        if (
+            "number" not in b
+            or b["complete"] != True
+            or (builder != "builds/targets" and builder != "builds/packages")
+        ):
             return
 
         # pp = pprint.PrettyPrinter(indent=4)
         # log.msg(f'MatrixNotifier.sendMessage - {pp.pformat(reports)}')
 
-        version = b['properties']['branch'][0]
-        if builder == 'builds/targets':
-            version = b['properties']['falterVersion'][0]
+        version = b["properties"]["branch"][0]
+        if builder == "builds/targets":
+            version = b["properties"]["falterVersion"][0]
 
-        result = statusToString(b['results'])
-        color = '#ff0000'
-        if b['results'] == 0:
-            color = '#008000'
+        result = statusToString(b["results"])
+        color = "#ff0000"
+        if b["results"] == 0:
+            color = "#008000"
 
-        url = b['url']
+        url = b["url"]
 
-        msg = f'{builder} @ {version} · {result} · {url}'
-        htmlmsg = f'{builder} @ {version} · <font color="{color}">{result}</font> · {url}'
+        msg = f"{builder} @ {version} · {result} · {url}"
+        htmlmsg = (
+            f'{builder} @ {version} · <font color="{color}">{result}</font> · {url}'
+        )
 
         http = yield httpclientservice.HTTPClientService.getService(
-            self.master, self.homeserver)
+            self.master, self.homeserver
+        )
 
         res = http.post(
-            f'/_matrix/client/r0/rooms/{self.room}/send/m.room.message?access_token={self.accessToken}',
-            json={'msgtype':'m.notice', 'body':msg,
-                'format':'org.matrix.custom.html', 'formatted_body':htmlmsg})
-
+            f"/_matrix/client/r0/rooms/{self.room}/send/m.room.message?access_token={self.accessToken}",
+            json={
+                "msgtype": "m.notice",
+                "body": msg,
+                "format": "org.matrix.custom.html",
+                "formatted_body": htmlmsg,
+            },
+        )
--- roles/buildbot/files/packages.py
+++ roles/buildbot/files/packages.py
@@ -1,80 +1,107 @@
 # -*- python -*-
 # ex: set filetype=python:
 
-from buildbot.plugins import *
 import re
 
 from asyncbuild import *
+from buildbot.plugins import *
+
 
 def packagesConfig(c, config):
+    c["schedulers"].append(
+        schedulers.Triggerable(name="dummy/packages", builderNames=["dummy/packages"])
+    )
 
-  c['schedulers'].append(schedulers.Triggerable(
-    name="dummy/packages",
-    builderNames=["dummy/packages"]))
+    c["schedulers"].append(
+        schedulers.ForceScheduler(
+            name="force-packages",
+            buttonName="Build Package Feed",
+            builderNames=["builds/packages"],
+            codebases=[
+                util.CodebaseParameter(
+                    "",
+                    label="Build falter package feeds using falter-packages.git",
+                    branch=util.ChoiceStringParameter(
+                        name="branch",
+                        label="git branch",
+                        choices=config["packages_branches"],
+                        default="master",
+                        strict=True,
+                    ),
+                    revision=util.FixedParameter(name="revision", default=""),
+                    repository=util.FixedParameter(
+                        name="repository", default=config["packages_repo"]
+                    ),
+                    project=util.FixedParameter(name="project", default=""),
+                )
+            ],
+        )
+    )
 
-  c['schedulers'].append(schedulers.ForceScheduler(
-    name="force-packages",
-    buttonName="Build Package Feed",
-    builderNames=["builds/packages"],
-    codebases=[
-      util.CodebaseParameter(
-        "",
-        label="Build falter package feeds using falter-packages.git",
-        branch=util.ChoiceStringParameter(
-          name="branch",
-          label="git branch",
-          choices=config['packages_branches'],
-          default="master",
-          strict=True),
-        revision=util.FixedParameter(name="revision", default=""),
-        repository=util.FixedParameter(name="repository", default=config['packages_repo']),
-        project=util.FixedParameter(name="project", default=""))]))
+    c["builders"].append(
+        util.BuilderConfig(
+            name="builds/packages",
+            workernames=["masterworker"],
+            factory=packagesFactory(util.BuildFactory(), config["publishDir"]),
+            collapseRequests=False,
+        )
+    )
 
-  c['builders'].append(util.BuilderConfig(
-    name="builds/packages",
-    workernames=["masterworker"],
-    factory=packagesFactory(util.BuildFactory(), config['publishDir']),
-    collapseRequests=False))
+    c["builders"].append(
+        util.BuilderConfig(
+            name="dummy/packages",
+            workernames=config["workerNames"],
+            factory=packagesArchFactory(
+                util.BuildFactory(),
+                config["publishDir"],
+                config["publishURL"],
+                config["alpineVersion"],
+            ),
+            collapseRequests=False,
+        )
+    )
 
-  c['builders'].append(util.BuilderConfig(
-    name="dummy/packages",
-    workernames=config['workerNames'],
-    factory=packagesArchFactory(util.BuildFactory(), config['publishDir'], config['publishURL'], config['alpineVersion']),
-    collapseRequests=False))
+    return c
 
-  return c
 
 # Passed by packagesFactory to AsyncBuildGenerator to be called for each arch.
 def archTriggerStep(arch):
-  return AsyncTrigger(
-    # Step name limit is 50 chars, longest is currently 33 chars:
-    # "trigger arm_cortex-a15_neon-vfpv4"
-    # See https://github.com/buildbot/buildbot/issues/3413
-    name=util.Interpolate("trigger %(kw:arch)s", arch=arch),
-    waitForFinish=True,
-    warnOnFailure=True,
-    schedulerNames=["dummy/packages"],
-    copy_properties=['repository', 'branch', 'revision', 'got_revision'],
-    set_properties={
-      'arch': arch,
-      'origbuildnumber': util.Interpolate("%(prop:buildnumber)s"),
-      # Builder name limit is 70 characters, longest is currently 48 chars:
-      # packages/openwrt-22.03/arm_cortex-a15_neon-vfpv4
-      # See https://github.com/buildbot/buildbot/pull/3957
-      'virtual_builder_name': util.Interpolate("packages/%(prop:branch)s/%(kw:arch)s", arch=arch),
-      'virtual_builder_tags': ["packages", util.Interpolate("%(prop:branch)s")],
-      })
+    return AsyncTrigger(
+        # Step name limit is 50 chars, longest is currently 33 chars:
+        # "trigger arm_cortex-a15_neon-vfpv4"
+        # See https://github.com/buildbot/buildbot/issues/3413
+        name=util.Interpolate("trigger %(kw:arch)s", arch=arch),
+        waitForFinish=True,
+        warnOnFailure=True,
+        schedulerNames=["dummy/packages"],
+        copy_properties=["repository", "branch", "revision", "got_revision"],
+        set_properties={
+            "arch": arch,
+            "origbuildnumber": util.Interpolate("%(prop:buildnumber)s"),
+            # Builder name limit is 70 characters, longest is currently 48 chars:
+            # packages/openwrt-22.03/arm_cortex-a15_neon-vfpv4
+            # See https://github.com/buildbot/buildbot/pull/3957
+            "virtual_builder_name": util.Interpolate(
+                "packages/%(prop:branch)s/%(kw:arch)s", arch=arch
+            ),
+            "virtual_builder_tags": ["packages", util.Interpolate("%(prop:branch)s")],
+        },
+    )
+
 
 # Only needed for publishing, see pubdir further below.
 # Apart from that only used for targets builds.
 @util.renderer
 def branchToFalterBranch(props):
-    o2f = { 'master':'snapshot',
-            'openwrt-23.05':'1.4.0-snapshot',
-            'openwrt-22.03':'1.3.0-snapshot',
-            'openwrt-21.02':'1.2.3-snapshot',
-            'testbuildbot':'testbuildbot' }
-    return o2f.get(props['branch'])
+    o2f = {
+        "master": "snapshot",
+        "openwrt-23.05": "1.4.0-snapshot",
+        "openwrt-22.03": "1.3.0-snapshot",
+        "openwrt-21.02": "1.2.3-snapshot",
+        "testbuildbot": "testbuildbot",
+    }
+    return o2f.get(props["branch"])
+
 
 # Fans out to one builder per arch and blocks for the results.
 def packagesFactory(f, wwwPrefix):
@@ -83,7 +110,9 @@ def packagesFactory(f, wwwPrefix):
         steps.SetProperty(
             name=util.Interpolate("falterBranch = %(kw:fv)s", fv=branchToFalterBranch),
             property="falterBranch",
-            value=branchToFalterBranch))
+            value=branchToFalterBranch,
+        )
+    )
     f.addStep(
         steps.Git(
             name="git clone",
@@ -91,69 +120,90 @@ def packagesFactory(f, wwwPrefix):
             repourl=util.Interpolate("%(prop:repository)s"),
             # if using incremental, we get strange behaviors, when configuring
             # another repo, that is back the other repo. Doing full checkouts is cleaner
-            method='clobber',
-            mode='full',
-            submodules=True))
+            method="clobber",
+            mode="full",
+            submodules=True,
+        )
+    )
     f.addStep(
-        AsyncBuildGenerator(archTriggerStep,
+        AsyncBuildGenerator(
+            archTriggerStep,
             name="generate builds",
             haltOnFailure=True,
-            command=["sh", "-c", util.Interpolate(
-                # Read our list of package architectures to build:
-                #
-                # 1. Print the architectures-to-targets mapping
-                # 2. Remove comments and empty lines
-                # 3. Take only the first column (delimited by a space)
-                #
-                # TODO: doesn't fail if targets-*.txt doesn't exist
-                """\
+            command=[
+                "sh",
+                "-c",
+                util.Interpolate(
+                    # Read our list of package architectures to build:
+                    #
+                    # 1. Print the architectures-to-targets mapping
+                    # 2. Remove comments and empty lines
+                    # 3. Take only the first column (delimited by a space)
+                    #
+                    # TODO: doesn't fail if targets-*.txt doesn't exist
+                    """\
 cat build/targets-%(prop:branch)s.txt \
 | grep -v '#' | grep . \
 | cut -d' ' -f1 \
-""")]))
+"""
+                ),
+            ],
+        )
+    )
 
     wwwdir = util.Interpolate(
-        "%(kw:prefix)s/builds/packages/%(prop:buildnumber)s", prefix=wwwPrefix)
+        "%(kw:prefix)s/builds/packages/%(prop:buildnumber)s", prefix=wwwPrefix
+    )
     pubdir = util.Interpolate(
-        "%(kw:prefix)s/feed/%(prop:falterBranch)s/packages", prefix=wwwPrefix)
+        "%(kw:prefix)s/feed/%(prop:falterBranch)s/packages", prefix=wwwPrefix
+    )
     f.addStep(
         steps.MasterShellCommand(
             name="publish",
             haltOnFailure=True,
-            command=["sh", "-c", util.Interpolate(
-                # Publish build in a way that minimizes downtime.
-                #
-                # We call the currently published artifacts "current",
-                # which is e.g. /unstable/1.3.0-snapshot or /unstable/snapshot.
-                # We also use two temporary directories called "new" and "prev".
-                #
-                # 1. Remove "new" and "prev" leftovers from previous builds
-                # 2. Move build artifacts into "new"
-                # 3. Rename "current" published stuff to "prev"
-                # 4. Publish "new" by renaming it to "current"
-                # 5. Remove "prev"
-                #
-                # This is slightly different from targets builds,
-                # where we only move instead of copy+move.
-                # That means /builds/packages/%d is available after publishing,
-                # while it's empty for targets builds.
-                #
-                # Packages downloads are only unavailable after step 3 and
-                # before step 4 has completed.
-                #
-                # Just symlinking from /builds/packages/%d is not a good option,
-                # since we want to be able to just delete that at any time,
-                # without worrying about symlinks pointing to deleted stuff.
-                """\
+            command=[
+                "sh",
+                "-c",
+                util.Interpolate(
+                    # Publish build in a way that minimizes downtime.
+                    #
+                    # We call the currently published artifacts "current",
+                    # which is e.g. /unstable/1.3.0-snapshot or /unstable/snapshot.
+                    # We also use two temporary directories called "new" and "prev".
+                    #
+                    # 1. Remove "new" and "prev" leftovers from previous builds
+                    # 2. Move build artifacts into "new"
+                    # 3. Rename "current" published stuff to "prev"
+                    # 4. Publish "new" by renaming it to "current"
+                    # 5. Remove "prev"
+                    #
+                    # This is slightly different from targets builds,
+                    # where we only move instead of copy+move.
+                    # That means /builds/packages/%d is available after publishing,
+                    # while it's empty for targets builds.
+                    #
+                    # Packages downloads are only unavailable after step 3 and
+                    # before step 4 has completed.
+                    #
+                    # Just symlinking from /builds/packages/%d is not a good option,
+                    # since we want to be able to just delete that at any time,
+                    # without worrying about symlinks pointing to deleted stuff.
+                    """\
 mkdir -p %(kw:p)s %(kw:p)s.new \
     && cp -a %(kw:w)s/* %(kw:p)s.new/ \
     && mv %(kw:p)s %(kw:p)s.prev \
     && mv %(kw:p)s.new %(kw:p)s \
     && rm -rf %(kw:p)s.prev \
 """,
-                w=wwwdir, p=pubdir)]))
+                    w=wwwdir,
+                    p=pubdir,
+                ),
+            ],
+        )
+    )
 
     return f
+
 
 # Runs build.sh with prop:arch and prop:branch, and uploads the result to master.
 def packagesArchFactory(f, wwwPrefix, wwwURL, alpineVersion):
@@ -161,19 +211,22 @@ def packagesArchFactory(f, wwwPrefix, wwwURL, alpineVersion):
         steps.ShellCommand(
             name="build",
             haltOnFailure=True,
-            interruptSignal='TERM', # podman can't proxy the KILL signal
-            command=["sh", "-c", util.Interpolate(
-                # Container that prints a tarball to stdout, and build output to stderr.
-                #
-                # Parameters:
-                # * -i so we get output
-                # * no -t because it messes with stdout/stderr
-                # * --rm so we don't fill up the disk with old containers
-                # * --log-driver so we don't pump huge tarballs into the logging facility
-                #   * it'd also trigger a segfault in ~15% of concurrent runs:
-                #     https://github.com/containers/podman/issues/13779
-                #
-                """\
+            interruptSignal="TERM",  # podman can't proxy the KILL signal
+            command=[
+                "sh",
+                "-c",
+                util.Interpolate(
+                    # Container that prints a tarball to stdout, and build output to stderr.
+                    #
+                    # Parameters:
+                    # * -i so we get output
+                    # * no -t because it messes with stdout/stderr
+                    # * --rm so we don't fill up the disk with old containers
+                    # * --log-driver so we don't pump huge tarballs into the logging facility
+                    #   * it'd also trigger a segfault in ~15% of concurrent runs:
+                    #     https://github.com/containers/podman/issues/13779
+                    #
+                    """\
 podman run -i --rm --log-driver=none docker.io/library/alpine:%(kw:alpineVersion)s sh -c '\
 ( \
     apk add git bash wget xz gzip unzip grep diffutils findutils coreutils build-base gcc abuild binutils ncurses-dev gawk bzip2 perl python3 rsync argp-standalone musl-fts-dev musl-obstack-dev musl-libintl \
@@ -186,11 +239,18 @@ podman run -i --rm --log-driver=none docker.io/library/alpine:%(kw:alpineVersion
 ) >&2 \
 && cd /root/falter-packages/out/ \
 && tar -c *' > out.tar \
-""", alpineVersion=alpineVersion)]))
+""",
+                    alpineVersion=alpineVersion,
+                ),
+            ],
+        )
+    )
 
     tarfile = util.Interpolate("packages-%(prop:origbuildnumber)s-%(prop:arch)s.tar")
     wwwpath = util.Interpolate("builds/packages/%(prop:origbuildnumber)s/%(prop:arch)s")
-    wwwdir = util.Interpolate("%(kw:prefix)s/%(kw:wwwpath)s", prefix=wwwPrefix, wwwpath=wwwpath)
+    wwwdir = util.Interpolate(
+        "%(kw:prefix)s/%(kw:wwwpath)s", prefix=wwwPrefix, wwwpath=wwwpath
+    )
     wwwurl = util.Interpolate("%(kw:url)s/%(kw:wwwpath)s", url=wwwURL, wwwpath=wwwpath)
     f.addStep(
         steps.FileUpload(
@@ -199,34 +259,57 @@ podman run -i --rm --log-driver=none docker.io/library/alpine:%(kw:alpineVersion
             workersrc="out.tar",
             masterdest=tarfile,
             url=wwwurl,
-            urlText=wwwurl))
+            urlText=wwwurl,
+        )
+    )
     f.addStep(
         steps.MasterShellCommand(
             name="extract",
             haltOnFailure=True,
-            command=["sh", "-c", util.Interpolate(
-                "mkdir -vp %(kw:wwwdir)s && tar -v -C %(kw:wwwdir)s -xf %(kw:tarfile)s",
-                tarfile=tarfile, wwwdir=wwwdir)]))
+            command=[
+                "sh",
+                "-c",
+                util.Interpolate(
+                    "mkdir -vp %(kw:wwwdir)s && tar -v -C %(kw:wwwdir)s -xf %(kw:tarfile)s",
+                    tarfile=tarfile,
+                    wwwdir=wwwdir,
+                ),
+            ],
+        )
+    )
     f.addStep(
         steps.MasterShellCommand(
             name="sign",
             haltOnFailure=True,
-            command=["sh", "-c", util.Interpolate(
-                "signify-openbsd -S -m %(kw:wwwdir)s/falter/Packages -s packagefeed_master.sec",
-                wwwdir=wwwdir)]))
+            command=[
+                "sh",
+                "-c",
+                util.Interpolate(
+                    "signify-openbsd -S -m %(kw:wwwdir)s/falter/Packages -s packagefeed_master.sec",
+                    wwwdir=wwwdir,
+                ),
+            ],
+        )
+    )
     f.addStep(
         steps.MasterShellCommand(
             name="cleanup master",
             alwaysRun=True,
             warnOnFailure=False,
-            command=["sh", "-c", util.Interpolate(
-                "rm -vf %(kw:tarfile)s",
-                tarfile=tarfile)]))
+            command=[
+                "sh",
+                "-c",
+                util.Interpolate("rm -vf %(kw:tarfile)s", tarfile=tarfile),
+            ],
+        )
+    )
     f.addStep(
         steps.ShellCommand(
             name="cleanup worker",
             alwaysRun=True,
             warnOnFailure=False,
-            command=["sh", "-c", "rm -vf out.tar"]))
+            command=["sh", "-c", "rm -vf out.tar"],
+        )
+    )
 
     return f
