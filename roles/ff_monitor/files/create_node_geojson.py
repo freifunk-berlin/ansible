eaad719ef9272deb19d26292e302563d8bb2af1c
Nicolas Berens <kontakt@nicolas-berens.de>
2025-07-21 22:41:12 +0200

    Cleanup (#151)
    * update with upstream
    
    * compatibility with debian13 and we do firewalling now on hypervisor level
    
    * allow access to api
    
    * remove old nginx config
    
    * remove unused roles and hosts and update the rest

--- roles/ff_monitor/files/create_node_geojson.py
+++ roles/ff_monitor/files/create_node_geojson.py
@@ -1,6 +1,20 @@
 #!/bin/env python3
 import json
 import urllib.request
+import requests
+
+PROMETHEUS_HOST="localhost:9090"
+
+dhcp_leases=requests.get('http://localhost:9090/api/v1/query', params={'query': 'collectd_dhcpleases_count{}'})
+
+LEASES_DICT = dict()
+
+for node in dhcp_leases.json()['data']['result']:
+    node_name = node['metric']['exported_instance']
+    leases = node['value'][1]
+    LEASES_DICT[node_name] = leases
+
+
 
 with urllib.request.urlopen("https://hopglass.berlin.freifunk.net/nodes.json") as url:
     data = json.loads(url.read().decode())
@@ -16,6 +30,8 @@ with urllib.request.urlopen("https://hopglass.berlin.freifunk.net/nodes.json") a
                                                      node['nodeinfo']['location']['latitude']]}
 
         properties['name'] = node['nodeinfo']['hostname']
+        properties['leases'] = LEASES_DICT.get(node['nodeinfo']['hostname'], 0)
+
         simplenode['properties'] = properties
         simplenode['geometry'] = geometry
 
